c4290752233fcb7fe390530c5cb13da2b9ed74b7
awitas <adrianwit@gmail.com>
2023-06-09 15:47:02 -0700

    added decoded resource

--- async/mbus/resource.go
+++ async/mbus/resource.go
@@ -42,3 +42,37 @@ func (r *Resource) Init() error {
 	}
 	return nil
 }
+
+//EncodedResource represents encoded resource
+type EncodedResource string
+
+func (e EncodedResource) Decode() (*Resource, error) {
+	ret := &Resource{}
+	parts := strings.Split(string(e), "/")
+	partLen := len(parts)
+	if partLen < 4 {
+		return nil, fmt.Errorf("faield to decode mbus resource: invalid format: %v, expected:name|vendor|resourceType|uri[|region|secretURL|secretKey]", e)
+	}
+	ret.Name = parts[0]
+	ret.Vendor = parts[1]
+	ret.Type = parts[2]
+	switch ret.Type {
+	case ResourceTypeTopic, ResourceTypeQueue, ResourceTypeSubscription:
+	default:
+		return nil, fmt.Errorf("invalid resource: type: %v, expected:%v", []string{ResourceTypeTopic, ResourceTypeQueue, ResourceTypeSubscription})
+	}
+	ret.URL = parts[3]
+	if partLen > 4 {
+		ret.Region = parts[4]
+	}
+
+	if partLen > 5 {
+		ret.Credentials = &scy.Resource{
+			URL: parts[5],
+		}
+		if partLen > 6 {
+			ret.Credentials.Key = parts[6]
+		}
+	}
+	return ret, nil
+}
